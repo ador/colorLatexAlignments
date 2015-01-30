8763aee096a284fbc6b65d5d8575d2f58197c75e
Adrienn Szabo <adrienn.szabo4@gmail.com>
2015-01-30 11:23:22 +0100

    fix

--- cleanup_tests_all.sh
+++ cleanup_tests_all.sh
@@ -4,7 +4,7 @@ baseDir="$(dirname "$0")"
 
 pushd "$baseDir" > /dev/null
 
-  ./cleanup.sh
+  ./cleanup_tests.sh
   rm generated_sample.tex 2>/dev/null
   rm generated_align_document.pdf 2>/dev/null
   rm generated_abc_align_document.pdf 2>/dev/null
