b5d2c01bd6dbbfc28e1c453c7c8c355a926ba2f4
Adrienn Szabo <adrienn.szabo4@gmail.com>
2015-01-30 14:30:36 +0100

    moving color definintion files

--- example2.sh
+++ example2.sh
@@ -6,7 +6,7 @@ export PYTHONPATH="$thisAbsDir:${PYTHONPATH:-}"
 
 pushd "$baseDir" > /dev/null
 
-  python3 coloraligns/apply_colors.py -i sampledata/conus-example-aligned.fasta -c testdata/sample_colordef.txt -l 38 -n 14 --small=True -o conus_sample.tex
+  python3 coloraligns/apply_colors.py -i sampledata/conus-example-aligned.fasta -c sampledata/colordef.txt -l 38 -n 14 --small=True -o conus_sample.tex
 
   pdflatex conus_sample.tex
 
--- test_latex.sh
+++ test_latex.sh
@@ -4,10 +4,10 @@ baseDir="$(dirname "$0")"
 
 pushd "$baseDir" > /dev/null
 
-  python3 coloraligns/apply_colors.py -i testdata/sample_align_short3.fsa -c testdata/sample_colordef.txt -l 20 -n 10 -o generated_sample_doc.tex
+  python3 coloraligns/apply_colors.py -i testdata/sample_align_short3.fsa -c sampledata/colordef.txt -l 20 -n 10 -o generated_sample_doc.tex
   pdflatex generated_sample_doc.tex
 
-  python3 coloraligns/apply_colors.py -i testdata/sample_abc_align.fsa -c testdata/sample_colordef.txt -l 20 -n 10 -o generated_abc_doc.tex
+  python3 coloraligns/apply_colors.py -i testdata/sample_abc_align.fsa -c sampledata/colordef.txt -l 20 -n 10 -o generated_abc_doc.tex
   pdflatex generated_abc_doc.tex
 
 popd > /dev/null
