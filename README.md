0043e82b66e2c4cd0e891fe46b7d31837023954e
Adrienn Szabo <adrienn.szabo4@gmail.com>
2015-01-30 15:09:17 +0100

    readme update and better cleanup

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Run the tests in the top level directory within the git repository to check if e
 
 How to use
 ----------
-See the example scripts: `example1.sh` and `example2.sh`.
+See the example scripts: `example1.sh` and `example2.sh` and `example3.sh`.
 
 Or print the help menu:<br />
 `$ python3 coloraligns/apply_colors.py --help`
--- cleanup_tests_all.sh
+++ cleanup_tests_all.sh
@@ -7,6 +7,12 @@ pushd "$baseDir" > /dev/null
   ./cleanup_tests.sh
   rm generated_*.tex 2>/dev/null
   rm generated_*.pdf 2>/dev/null
+  rm conus_sample1.tex 2>/dev/null
+  rm conus_sample1.pdf 2>/dev/null
+  rm conus_sample2.tex 2>/dev/null
+  rm dna_sample.pdf 2>/dev/null
+  rm dna_sample.tex 2>/dev/null
+  rm conus_sample.pdf 2>/dev/null
   rm *.aux 2>/dev/null
   rm *.log 2>/dev/null
 
